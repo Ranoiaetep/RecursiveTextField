97ef741cf61449be46a1ce838f703ac89c1e56fa
Ranoiaetep <ranoiaetep@me.com>
2021-02-01 01:26:46 -0800

    Made objects public

--- Sources/RecursiveTextField/RecursiveTextField.swift
+++ Sources/RecursiveTextField/RecursiveTextField.swift
@@ -1,20 +1,20 @@
 import SwiftUI
 
-struct RecursiveTextFieldView: View {
-	@Binding var textList: [String]
-	var index: Int = 0
-	var placeholder: String = "Placeholder"
+public struct RecursiveTextField: View {
+	@Binding public var textList: [String]
+	public var index: Int = 0
+	public var placeholder: String = "Placeholder"
 	@State private var skipped: Bool = false
 	@State private var nextRecursion: AnyView?
 	
-	var body: some View {
+	public var body: some View {
 		if !skipped {
 			TextField(placeholder, text: $textList[index], onCommit: {
 				if !textList[index].isEmpty {
 					if !(textList.last?.isEmpty ?? false) {
 						textList.append(String())
 					}
-					nextRecursion = AnyView(RecursiveTextFieldView(textList: $textList, index: index + 1, placeholder: placeholder))
+					nextRecursion = AnyView(RecursiveTextField(textList: $textList, index: index + 1, placeholder: placeholder))
 				} else if index != textList.count - 1 {
 					skipped = true
 				}
